2d7f6c899d7a3410943ef81de8536a8037752bec
BuildTools <unconfigured@null.spigotmc.org>
2025-02-10 16:47:23 -0500

    simulation of larger neighborhoods

--- bin/test.jl
+++ bin/test.jl
@@ -236,7 +236,6 @@ recursive_index( v::Vector{Float64}, s, i, j )::Vector{Float64} = v
 
 recursive_index( a, s, i, j )::Vector{Float64} = recursive_index( a[s[i, j]], s, i, j+1 )
 
-
 simulate( neighbor_dependencies, m, n; seed=1 ) =
     let sizes = size( neighbor_dependencies )
         simulate( separate( cumsum( neighbor_dependencies, dims=length(sizes) ) ), sizes, m, n, seed=seed )
@@ -268,5 +267,23 @@ function simulate( cums::Vector, sizes, m, n; seed=1 )
 end
 
 s = simulate( neighbor_dependencies, 1_000_000, 100 );
+s = simulate( neighbor_dependencies, 1_000_000, 100 );
+@time s = simulate( neighbor_dependencies, 1_000_000, 100 );
+
+recat_inner( x ) = if isbitstype(eltype(x))
+    x
+else
+    let y = recat_inner.( x )
+        cat( y..., dims=length(size(y[1]))+1 )
+    end
+end
+
+recat( x ) = let y = recat_inner( x )
+    permutedims( y, length(size(y)):-1:1 )
+end
+
+neighbor_dependencies = recat( [[[0.99 0.01; 0.98 0.02], [0.03 0.97; 0.04 0.96]], [[0.95 0.05; 0.94 0.06], [0.07 0.93; 0.08 0.92]]] )
+neighbor_dependencies = recat( [[[0.99 0.01; 0.98 0.02]', [0.03 0.97; 0.04 0.96]'], [[0.95 0.05; 0.94 0.06]', [0.07 0.93; 0.08 0.92]']] )
+
 @time s = simulate( neighbor_dependencies, 1_000_000, 100 );
 
